52604d9187dfe2980e95181ba124b9f59b45f6f7
Pomelo4455 <sm67588@gmail.com>
2022-12-09 09:11:48 -0300

    Last commit

--- client/src/redux/store/index.js
+++ client/src/redux/store/index.js
@@ -1,7 +1,20 @@
-import { applyMiddleware, createStore } from "redux";
+/*import { applyMiddleware, createStore } from "redux";
 import thunk from "redux-thunk";
 import reducer from "../reducer";
 
 const store = createStore(reducer, applyMiddleware(thunk));
+
+export default store;*/
+
+
+import { applyMiddleware, createStore } from "redux";
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from "redux-thunk";
+import reducer from "../reducer";
+
+const store = createStore( reducer,
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    composeWithDevTools(applyMiddleware(thunk))
+);
 
 export default store;
